3f13abbc0de1fc0f1a5f54e0f7844bf9f49a898c
EeeMoonJR <eeemoonart@gmail.com>
2023-12-27 20:14:35 +0200

    small changes

--- pinkie/__init__.py
+++ pinkie/__init__.py
@@ -1,2 +1,3 @@
 from .rgba import *
 from .hsla import *
+from .blend import *
--- pinkie/blend.py
+++ pinkie/blend.py
@@ -1,15 +1,36 @@
 class BlendMode:
+    """
+    Base class for blending mode. 
+    Inherited classes should only have overwritten `blend()` method.
+    """
+    def __init__(self, bits: int) -> None:
+        self.bits = bits
+        self.max_one = 2 ** bits - 1
+        self.max_all = 2 ** (bits * 4) - 1
+
     def blend(self, bg, fg):
         raise NotImplementedError()
     
-    def compose(self, bg, fg, bits: int):
+    def compose(self, bg, fg):
         if bg._bits != fg._bits:
             raise ValueError(f"can not blend colors with different size")
         
         return self.blend(bg, fg)
         
     
-
 class Normal(BlendMode):
+    """
+    Normal blend mode.
+    """
     def blend(self, bg, fg):
-        return fg
+        from .rgba import RGBA
+
+        alpha = fg.a / self.max_one
+
+        return RGBA((
+            (fg.r * alpha + bg.r * (1 - alpha)),
+            (fg.g * alpha + bg.g * (1 - alpha)),
+            (fg.b * alpha + bg.b * (1 - alpha)),
+            max(fg.a, bg.a)
+        ), self.bits)
+    
